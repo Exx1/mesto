7d675d9b58cbd24674bc41157b78f8c49a4c78af
Sergei Abramenkov <exx-3@yandex.ru>
2023-05-22 11:31:33 +0300

    fix: styles

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@ let name = document.querySelector(".profile__text");
 let status = document.querySelector(".profile__status");
 let nameForm = document.querySelector(".popup__input_type_name");
 let statusForm = document.querySelector(".popup__input_type_status");
-let saveButton = document.querySelector(".popup__button");
+let form = document.querySelector(".popup__form");
 
 
 
@@ -14,23 +14,22 @@ function fillForm () {
   statusForm.value = status.innerText;
 }
 
-fillForm();
-
 function fillFormOut(evt) {
   evt.preventDefault();
   name.textContent = nameForm.value;
   status.textContent = statusForm.value;
-  closeEdit();
+  toggleEdit();
 }
 
-function openEdit () {
-  editForm.classList.toggle('popup_opened');
+function toggleEdit () {
+  if (editForm.classList.contains('popup_opened')) {
+    editForm.classList.toggle('popup_opened')
+  } else {
+    editForm.classList.toggle('popup_opened');
+    fillForm();
+  }
 }
 
-function closeEdit () {
-  editForm.classList.toggle('popup_opened');
-}
-
-editButton.addEventListener('click', openEdit);
-closeButton.addEventListener('click', closeEdit);
-saveButton.addEventListener('click', fillFormOut);
+editButton.addEventListener('click', toggleEdit);
+closeButton.addEventListener('click', toggleEdit);
+form.addEventListener('submit', fillFormOut);
