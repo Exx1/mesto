82dfd5b8c41fa41768e10ea13f0df73ea20b5a19
Sergei Abramenkov <exx-3@yandex.ru>
2023-10-08 16:58:15 +0300

    добавил класс PopupWithorm

--- scripts/PopupWithForm.js
+++ scripts/PopupWithForm.js
@@ -4,17 +4,30 @@ export default class PopupWithForm extends Popup {
   constructor(selectorPopup, submitForm) {
     super(selectorPopup);
     this._submitForm = submitForm;
+    this._formAddCard = this._popup.querySelector(".popup__form");
   }
 
   _getInputValues() {
 
+    this._inputList = this._popup.querySelectorAll('.popup__input');
+    this._formValues = {};
+    this._inputList.forEach(input => {
+      this._formValues[input.name] = input.value;
+    });
+    return this._formValues;
   }
 
   setEventListeners() {
+    this._formAddCard.addEventListener('submit', (evt) => {
+      evt.preventDefault();
 
+      this._submitForm(this._getInputValues());
+    });
   }
 
   close() {
-
+    this._formAddCard.reset();
+    this._popup.classList.remove('popup_opened');
+    this._removeEventListeners();
   }
 }
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,7 @@ import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
 import Section from "./Section.js";
 import PopupWithImage from "./PopupWithImage.js";
+import PopupWithForm from "./PopupWithForm.js";
 
 const validationConfig = {
   formSelector: '.popup__form',
@@ -15,7 +16,7 @@ const validationConfig = {
 
 const buttonEditProfile = document.querySelector(".profile__edit-button");
 const popupEditProfile = document.querySelector(".popup-edit-profile");
-const popupAddCard = document.querySelector(".popup-add-card");
+const popupAddCardSelector = ".popup-add-card";
 const buttonCloseEditProfile = document.querySelector(".popup__close_edit");
 const buttonCloseAddCard = document.querySelector(".popup__close_add");
 const nameProfile = document.querySelector(".profile__text");
@@ -25,24 +26,12 @@ const inputStatusFormEditProfile = document.querySelector(".popup__input_type_st
 const inputTitleFormAddCard = document.querySelector(".popup__input_type_title");
 const inputLinkFormAddCard = document.querySelector(".popup__input_type_link");
 const formEditProfile = document.querySelector(".popup__form_edit-profile");
-const formAddCard = document.querySelector(".popup__form_add-card");
 const buttonAddCard = document.querySelector('.profile__button-add-card');
+const formAddCardSelector = document.querySelector(".popup__form_add-card");
 const popupFullSelector = '.popup-image';
 const elements = '.elements';
 
 
-
-function openPopup(popup) {
-  popup.classList.add('popup_opened');
-  popup.addEventListener('click', closePopupClickOverlay);
-  document.addEventListener('keydown', closePopupKeydownEsc);
-}
-
-function closePopup(popup) {
-  popup.removeEventListener('click', closePopupClickOverlay);
-  document.removeEventListener('keydown', closePopupKeydownEsc);
-  popup.classList.remove('popup_opened');
-}
 
 function fillFormEditProfile() {
   inputNameFormEditProfile.value = nameProfile.innerText;
@@ -57,6 +46,7 @@ function submitEditProfileForm(evt) {
 }
 
 const popupFull = new PopupWithImage(popupFullSelector);
+const formAddCard = new PopupWithForm(popupAddCardSelector, addCard);
 
 const cardList = new Section({items: initialCards, renderer: (item) => {
   const card = new Card(item, '#elements_template', handleCardClick);
@@ -86,9 +76,7 @@ function addCard(evt) {
 
   cardNew.renderer();
 
-  closePopup(popupAddCard);
-
-  formAddCard.reset();
+  formAddCard.close();
 
   cardFormValidator.resetValidation();
 }
@@ -105,21 +93,18 @@ buttonCloseEditProfile.addEventListener('click', function () {
 formEditProfile.addEventListener('submit', submitEditProfileForm);
 
 buttonAddCard.addEventListener('click', function () {
-  openPopup(popupAddCard);
+  formAddCard.open();
 });
 buttonCloseAddCard.addEventListener('click', function () {
-  closePopup(popupAddCard);
+  formAddCard.close();
 });
 
-
-
-formAddCard.addEventListener('submit', addCard);
 
 function handleCardClick(name, link) {
   popupFull.open(name, link);
 }
 
-const cardFormValidator = new FormValidator(validationConfig, formAddCard);
+const cardFormValidator = new FormValidator(validationConfig, formAddCardSelector);
 cardFormValidator.enableValidation();
 
 const profileFormValidator = new FormValidator(validationConfig, formEditProfile);
